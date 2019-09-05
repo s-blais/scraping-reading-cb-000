8f6c2af84103ea30c374bf4862b8e390a130310f
Stephen Blais <stephen@s-blais.com>
2019-09-05 22:48:10 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,5 +3,9 @@ require 'open-uri'
 
 html = open("https://flatironschool.com/")
 
+doc = Nokogiri::HTML(open("https://flatironschool.com/"))
+scrapes = doc.css(".title-oE5vT4")
 
-
+scrapes.each do |scrape|
+  puts scrape.text.strip
+end
