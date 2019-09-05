d84a67d25a83c6fa1bd9c67417c9f992f5bba1e6
Stephen Blais <stephen@s-blais.com>
2019-09-05 22:29:45 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,6 +3,5 @@ require 'open-uri'
 
 html = open("https://flatironschool.com/")
 
-doc = Nokogiri::HTML(html)
-
-puts doc
+doc = Nokogiri::HTML(open("https://flatironschool.com/"))
+puts doc.css(".headline-26OIBN").text.strip
